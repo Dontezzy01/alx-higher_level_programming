9b1d21e0614cb80d006694570a4057fb65169781
Sulaimon Quadri Dolapo <dontezzy01@gmail.com>
2022-11-30 09:32:25 -0800

    tasks

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -15,4 +15,5 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif number % 5 == 0:
             print("Buzz ", end="")
-
+        else:
+            print("{} ".format(number), end="")
